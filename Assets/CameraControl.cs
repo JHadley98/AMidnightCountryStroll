467c06f45b6ccc2289fd8d4655de982d68f5eeb7
MHamilton <32247409+cyberboy379@users.noreply.github.com>
2020-02-25 12:26:22 +0000

    Sheep And flocking stuff

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    float Pitch;
+    float Yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Yaw += Input.GetAxisRaw("Mouse X");
+        Pitch -= Input.GetAxisRaw("Mouse Y");
+        transform.eulerAngles = new Vector3(Pitch, Yaw, 0.0f);
+        transform.parent.Rotate(0, Input.GetAxisRaw("Mouse X") * 0.5f, 0);
+    }
+}
