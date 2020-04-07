041e09cac3ec36f0262d6c4ccb8ed17f9968b24f
KathrynSchofield <47246395+KathrynSchofield@users.noreply.github.com>
2020-04-07 18:06:39 +0100

    Game scene and unfinished decision making
    decision making unfashioned, unsure how to link everything together

---
+++ DecisionMaking.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionMaking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(GameObject.Find("Player").transform.position, GameObject.Find("Sheep").transform.position) <= 7)
+        {
+            chase();
+        }
+        else
+        {
+            sleep();
+        }
+
+        if (Vector3.Distance(GameObject.Find("Player").transform.position,GameObject.Find("Sheep").transform.position) <= 2)
+        {
+            attack();
+        }
+    }
+
+    void sleep()
+    {
+        print("sleepy sheep");
+        
+    }
+
+    void chase()
+    {
+        print("sheep chasing");
+        //gameObject.GetComponent<SheepControl>();?
+
+    }
+
+    void attack()
+    {
+        print("sheep attacking");
+    }
+
+
+}
+
+
