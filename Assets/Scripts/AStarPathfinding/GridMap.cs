a58df039d819ecf0cd2b51f9e6807066211b2b3f
James Hadley <james.hadley1@live.co.uk>
2020-04-16 14:22:33 +0100

    A* pathfinding added to game scene. Issue with Sheep Control class

---
+++ Assets/Scripts/AStarPathfinding/GridMap.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMap : MonoBehaviour
+{
+    public LayerMask _obstacle;
+    [SerializeField] private Vector2 _worldSize = new Vector2();
+    private readonly float _nodeSize = 1.0f;
+    Node[,] _gridMap;
+
+    private int _gridSizeX, _gridSizeY;
+
+    private void Start()
+    {
+        // Calculate how many nodes can fit across the world size
+        _gridSizeX = Mathf.RoundToInt(_worldSize.x / _nodeSize);
+        _gridSizeY = Mathf.RoundToInt(_worldSize.y / _nodeSize);
+
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        _gridMap = new Node[_gridSizeX, _gridSizeY];
+        // Get bottom left of world, .forward is used to get the Z axis in 3D space
+        Vector3 bottomLeft = transform.position - Vector3.right * _worldSize.x / 2 - Vector3.forward * _worldSize.y / 2;
+
+        for (int x = 0; x < _gridSizeX; x++)
+        {
+            for (int y = 0; y < _gridSizeY; y++)
+            {
+                // Get each point a node will occupy in the world
+                Vector3 worldPoint = bottomLeft + Vector3.right * (x * _nodeSize + (_nodeSize / 2)) + Vector3.forward * (y * _nodeSize + (_nodeSize / 2));
+                // Collision check
+                bool walkable = !(Physics2D.CircleCast(worldPoint, (_nodeSize / 2), Vector3.forward, (_nodeSize / 2), _obstacle));
+                // Create new node
+                _gridMap[x, y] = new Node(walkable, worldPoint, x, y);
+            }
+        }
+    }
+
+    public List<Node> GetNeighbouringNodes(Node node)
+    {
+        List<Node> neighbouringNodes = new List<Node>();
+
+        // Loop search in a 3x3 block
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+
+                // Find neighouring positions on x and y axis
+                int neighbourX = node._gridPositionX + x;
+                int neighbourY = node._gridPositionY + y;
+
+                // Check if neighbour is in grid
+                if (neighbourX >= 0 && neighbourX < _gridSizeX && neighbourY >= 0 && neighbourY< _gridSizeY)
+                {
+                    neighbouringNodes.Add(_gridMap[neighbourX, neighbourY]);
+                }
+            }
+        }
+
+        return neighbouringNodes;
+    }
+
+    public Node FindNodeInGrid(Vector3 gridPosition)
+    {
+        // Convert world position into a percentage for both x and y coordinate, to see how far along the node is in the grid
+        float percentageX = (gridPosition.x / _worldSize.x) + 0.5f;
+        float percentageY = (gridPosition.z / _worldSize.y) + 0.5f;
+
+        // Clamp percentage between 0 and 1
+        percentageX = Mathf.Clamp01(percentageX);
+        percentageY = Mathf.Clamp01(percentageY);
+
+        // Get x and y indices of the grid array
+        int x = Mathf.RoundToInt((_gridSizeX - 1) * percentageX);
+        int y = Mathf.RoundToInt((_gridSizeY - 1) * percentageY);
+
+        return _gridMap[x, y];
+    }
+
+    public List<Node> path;
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(_worldSize.x, 1, _worldSize.y));
+
+        if (_gridMap != null)
+        {
+            foreach (Node node in _gridMap)
+            {
+                Gizmos.color = (node._walkable) ? Color.white : Color.red;
+
+                // Display path between sheep and play in scene
+                if (path != null)
+                {
+                    // If path contains in the node then colour path black
+                    if(path.Contains(node))
+                    {
+                        Gizmos.color = Color.black;
+                    }
+                }
+
+                Gizmos.DrawCube(node._mapPosition, Vector3.one * (_nodeSize - 0.1f));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SheepControl.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepControl : MonoBehaviour
+{
+    public Transform Player;
+
+
+    
+    public int FlockAffectDistance;
+    public bool ActiveAlignment;
+    public bool ActiveCohesion;
+    [Range(0, 10)]
+    public int MinCohesionDist;
+    public bool ActiveSeparation;
+    [Range(1, 10)]
+    public int SeparationStrength;
+
+    Vector3 Velocity;
+    Vector3 Acceleration;
+    Vector3 Forward;
+
+    [Range(1,10)]
+    public int Speed;
+
+    public int AlertDistance;
+    public bool Alerted = false;
+    public float CrowdRadius;
+    GameObject[] Sheep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sheep = GameObject.FindGameObjectsWithTag("Sheep");
+        print(Sheep.Length);
+        Forward = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            SeparationStrength++;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            SeparationStrength--;
+        }
+
+        Acceleration = Vector3.zero;
+        if(!Alerted)
+        {
+            //transform.rotation = Quaternion.LookRotation(Player.transform.position - transform.position, Vector3.up);
+            if ((Player.position - transform.position).magnitude <= AlertDistance)
+                Alerted = true;
+        }
+        else
+        {
+            Vector3 SeparationEffect = Vector3.zero;
+            Vector3 AlignmentEffect = Vector3.zero;
+            Vector3 CohesionEffect = Vector3.zero;
+            int NumAffecting = -1;
+            
+
+            foreach (GameObject thisSheep in Sheep)
+            {
+                if ((thisSheep.transform.position - transform.position).magnitude < FlockAffectDistance)
+                {
+                    SheepControl otherSheepControl = thisSheep.GetComponent<SheepControl>();
+                    if (Alerted && !otherSheepControl.Alerted)
+                        otherSheepControl.Alerted = true;
+                        
+
+                    CohesionEffect += new Vector3(thisSheep.transform.position.x, 0, thisSheep.transform.position.z);
+                    
+                    NumAffecting++;
+                    
+
+                    //Separation Code Begins
+                    if((thisSheep.transform.position - transform.position).magnitude < CrowdRadius && (thisSheep.transform.position - transform.position).magnitude > 0)
+                    {
+                        Vector3 Difference = transform.position - thisSheep.transform.position;
+
+                        Difference.Normalize();
+                        Difference /= (thisSheep.transform.position - transform.position).magnitude * (thisSheep.transform.position - transform.position).magnitude;
+                        SeparationEffect += Difference;
+
+                    }
+                    //Separation Code Ends
+
+                    AlignmentEffect += new Vector3(thisSheep.transform.forward.x, 0, thisSheep.transform.forward.z);
+                }
+
+            }
+            if (ActiveCohesion)
+            {
+                //CohesionEffect -= transform.position;
+
+                CohesionEffect = (CohesionEffect / NumAffecting);
+                if((CohesionEffect - transform.position).magnitude > MinCohesionDist)
+                    Acceleration += (CohesionEffect - transform.position);
+            }
+            if (ActiveSeparation)
+            {
+                Acceleration += SeparationEffect * SeparationStrength;
+            }
+            if(ActiveAlignment)
+            {
+                //AlignmentEffect /= NumAffecting;
+                Acceleration += (AlignmentEffect.normalized - Acceleration);
+            }
+
+            Velocity = Acceleration.normalized;
+            Velocity.y = 0;
+            if(Velocity != Vector3.zero)
+                Forward = Velocity.normalized;
+            transform.forward = Forward;
+            transform.position += Forward * Speed * Time.deltaTime;
+        }
+        
+
+
+    }
+
+    
+}
