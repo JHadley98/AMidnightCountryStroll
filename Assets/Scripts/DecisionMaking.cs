3084e1f4cff3c5e6a944df2c48878071a6d8bc18
KathrynSchofield <47246395+KathrynSchofield@users.noreply.github.com>
2020-05-04 18:38:29 +0100

    Decision making

--- Assets/Scripts/DecisionMaking.cs
+++ Assets/Scripts/DecisionMaking.cs
@@ -4,46 +4,59 @@ using UnityEngine;
 
 public class DecisionMaking : MonoBehaviour
 {
+    public GameObject uiObjectLose;
+    public GameObject uiObjectWin;
+    GameObject[] Sheep;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiObjectLose.SetActive(false);
+        uiObjectWin.SetActive(false);
+        Sheep = GameObject.FindGameObjectsWithTag("Sheep");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(GameObject.Find("Player").transform.position, GameObject.Find("Sheep").transform.position) <= 7)
-        {
-            chase();
-        }
-        else
-        {
-            sleep();
-        }
+        
+        foreach(GameObject sheep in Sheep)
+        { 
+            if (Vector3.Distance(sheep.transform.position, GameObject.Find("Player").transform.position) <= 3)//check for close distance between player and sheep
+            {
+                lose();//when the sheep get too close to the player
+                break;
+            }
+            
+            }
+        
+               
+            if (Vector3.Distance(GameObject.Find("Player").transform.position, GameObject.Find("Path").transform.position) <= 4)
+            {
+                win(); //when player is at the end position
+            }
+        
 
-        if (Vector3.Distance(GameObject.Find("Player").transform.position,GameObject.Find("Sheep").transform.position) <= 2)
-        {
-            attack();
-        }
     }
+    
 
-    void sleep()
+    void win()
     {
-        print("sleepy sheep");
+        uiObjectWin.SetActive(true); //game is won
+
+        new WaitForSeconds(10);
+        Application.Quit();
         
     }
+    
 
-    void chase()
+    void lose()
     {
-        print("sheep chasing");
-        //gameObject.GetComponent<SheepControl>();?
-
-    }
-
-    void attack()
-    {
-        print("sheep attacking");
+        uiObjectLose.SetActive(true); //game is lost
+        new WaitForSeconds(10);
+        Application.Quit();
+        
     }
 
 
